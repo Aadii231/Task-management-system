9b403b899091f592ca5f3b48929ad5e9db9877ca
Aadii231 <adnanshakeel531@gmail.com>
2024-03-23 08:03:19 -0700

    its 3rd commit

--- app/Models/task.php
+++ app/Models/task.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class task extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description', 'priority', 'status', 'due_date'];
+    protected $fillable = ['title', 'description', 'priority', 'status', 'due_date','user_id'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
 
